0ab81f1773c377636b3487de9178591d229b3887
TihomirShterev <tisho_92@abv.bg>
2020-12-11 11:13:34 +0200

    Fixed CORS error

--- REST-API/index.js
+++ REST-API/index.js
@@ -6,6 +6,12 @@ const cors = require("cors");
 const app = express();
 const appString = `Server is ready, listening on port: ${port}...`;
 
+app.use((req, res, next) => {
+  // res.header('Access-Control-Allow-Origin', 'http://localhost:4200/');
+  res.header('Access-Control-Allow-Origin', '*'); // to work for all routes
+  next();
+});
+
 require("./config/database")()
   .then(() => {
     require("./config/express")(express, app);
