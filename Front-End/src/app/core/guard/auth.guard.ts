0f9e7a7af0fb8a4733819bc17c9cfd3636241a0b
TihomirShterev <tisho_92@abv.bg>
2020-12-13 07:58:02 +0200

    auth.guard and user.service update

--- Front-End/src/app/core/guard/auth.guard.ts
+++ Front-End/src/app/core/guard/auth.guard.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivateChild, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { IUser } from 'src/app/shared/interfaces';
 import { UserService } from 'src/app/user/user.service';
 
 @Injectable()
@@ -10,15 +12,29 @@ export class AuthGuard implements CanActivateChild {
     private userService: UserService,
     private router: Router
   ) { }
-  canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-    const isLoggedFromData = childRoute.data.isLogged;
 
-    if (typeof isLoggedFromData === "boolean" && isLoggedFromData === this.userService.isLogged) {
-      return true;
-    }
+  canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+    let stream$: Observable<IUser | null>;
 
-    const url = this.router.url;
-    this.router.navigateByUrl(url);
-    return false;
+    // if (this.userService.currentUser === undefined) {
+    //   stream$ = this.userService.getCurrentUserProfile();
+    // } else {
+    //   stream$ = of(this.userService.currentUser);
+    // }
+
+    return stream$.pipe(
+      map(user => {
+        const isLoggedFromData = childRoute.data.isLogged;
+        return typeof isLoggedFromData !== "boolean" || isLoggedFromData === this.userService.isLogged;
+      }),
+      tap(canContinue => {
+        if (canContinue) {
+          return true;
+        }
+    
+        const url = this.router.url;
+        this.router.navigateByUrl(url);
+      })
+    );
   }
 }
--- Front-End/src/app/user/user.service.ts
+++ Front-End/src/app/user/user.service.ts
@@ -1,26 +1,47 @@
 import { Injectable } from '@angular/core';
 import { StorageService } from '../core/storage.service';
 import { Observable, of } from 'rxjs';
-import { delay } from 'rxjs/operators';
+import { catchError, delay, tap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { IUser } from '../shared/interfaces';
+
+const apiUrl = environment.apiUrl;
 
 @Injectable()
 export class UserService {
+  // isLogged = false;
+  currentUser: IUser | null;
 
-  isLogged = false;
+  get isLogged(): boolean { return !!this.currentUser; }
+  
+  // constructor(private storage: StorageService) {
+  constructor(private http: HttpClient) {
+    // constructor(private storage: StorageService) {
+    // this.isLogged = this.storage.getItem("isLogged");
+  }
 
-  constructor(private storage: StorageService) {
-    this.isLogged = this.storage.getItem("isLogged");
+  register(data: any): Observable<any> {
+    return this.http
+    .post(`${apiUrl}/user/register`, data, { withCredentials: true })
+    .pipe(tap((user: IUser) => this.currentUser = user));
   }
 
   login(data: any): Observable<any> {
-    this.isLogged = true;
-    this.storage.setItem("isLogged", true);
-    return of(data).pipe(delay(3000));
+    // this.isLogged = true;
+    // this.storage.setItem("isLogged", true);
+    // return of(data).pipe(delay(3000));
+    return this.http
+    .post(`${apiUrl}/users/login`, data, { withCredentials: true })
+    .pipe(tap((user: IUser) => this.currentUser = user));
   }
 
   logout(): Observable<any> {
-    this.isLogged = false;
-    this.storage.setItem("isLogged", false);
-    return of(null).pipe(delay(3000));
+    // this.isLogged = false;
+    // this.storage.setItem("isLogged", false);
+    // return of(null).pipe(delay(3000));
+    return this.http
+    .post(`${apiUrl}/users/logout`, {}, { withCredentials: true })
+    .pipe(tap(() => this.currentUser = null));;
   }
 }
