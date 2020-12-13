83fde5a3d964d0a1de64c1b01ca8e286495ab3d3
TihomirShterev <tisho_92@abv.bg>
2020-12-13 07:56:59 +0200

    Routing updated

--- Front-End/src/app/app-routing.module.ts
+++ Front-End/src/app/app-routing.module.ts
@@ -1,20 +1,33 @@
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/guard/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
     path: "",
-    pathMatch: "full",
-    redirectTo: "/home"
-  },
-  {
-    path: "home",
-    component: HomeComponent
-  },
-  {
-    path: "**",
-    component: PageNotFoundComponent
+    canActivateChild: [ AuthGuard ],
+    children: [
+      {
+        path: "",
+        pathMatch: "full",
+        redirectTo: "/home"
+      },
+      {
+        path: "home",
+        component: HomeComponent,
+        data: {
+          title: "HOME"
+        }
+      },
+      {
+        path: "**",
+        component: PageNotFoundComponent,
+        data: {
+          title: "404"
+        }
+      }
+    ]
   }
 ];
 
--- Front-End/src/app/item/item-routing.module.ts
+++ Front-End/src/app/item/item-routing.module.ts
@@ -1,30 +1,39 @@
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from '../core/guard/auth.guard';
 import { CreateComponent } from './create/create.component';
 import { DetailsComponent } from './details/details.component';
 import { ItemComponent } from './item/item.component';
 
 const routes: Routes = [
   {
-    path: 'item',
-    pathMatch: 'full',
-    component: ItemComponent,
-    data: {
-      title: 'ITEM'
-    }
-  },
-  {
-    path: 'item/create',
-    component: CreateComponent,
-    data: {
-      title: 'CREATE ITEM'
-    }
-  },
-  {
-    path: 'item/details/:id',
-    component: DetailsComponent,
-    data: {
-      title: 'ITEM DETAILS'
-    }
+    path: "item",
+    canActivateChild: [ AuthGuard ],
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        component: ItemComponent,
+        data: {
+          title: 'ITEM'
+        }
+      },
+      {
+        path: 'create',
+        component: CreateComponent,
+        data: {
+          title: 'CREATE ITEM',
+          isLogged: true
+        }
+      },
+      {
+        path: 'details/:id',
+        component: DetailsComponent,
+        data: {
+          title: 'ITEM DETAILS',
+          isLogged: true
+        }
+      }
+    ]
   }
 ];
 
